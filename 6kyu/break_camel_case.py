31ebdf8333dcff168a8bccbcf35d98e8e609e5f2
cami <camilatienelabieberfever@gmail.com>
2022-02-14 23:20:28 -0300

    Adding break camel case

---
+++ 6kyu/break_camel_case.py
@@ -0,0 +1,15 @@
+problem = 'https://www.codewars.com/kata/5208f99aee097e6552000148'
+
+def solution(s):
+    upper = []
+    lower = []
+    for letter in s:
+        if letter.isupper():
+            upper.append(letter)
+            lower.append(' ')
+            lower.append('')
+        else:
+            lower.append(letter)
+    iter_upper = iter(upper)
+    r = [item if item else next(iter_upper) for item in lower]
+    return ''.join(r)
