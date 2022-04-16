0e0c5256159fd97cb98b1c6103ec5897f43d13e4
cami <camilatienelabieberfever@gmail.com>
2022-04-15 22:48:57 -0300

    Adding functional scramblies.py and its README

--- 5kyu/scramblies/scramblies_00.py
+++ 5kyu/scramblies/scramblies_00.py
@@ -4,7 +4,9 @@ def scramble(s1, s2):
     l = all(c in set(s1) for c in set(s2) )
     if not l:
         return False
-    return all(Counter(s1).get(k,0)//Counter(s2)[k] for k in Counter(s2))  
+    s1_c = Counter(s1)
+    s2_c = Counter(s2)
+    return all(s1_c.get(k,0)//s2_c[k] for k in s2_c)  
 
 w1,w2 = 'rkqodlw', 'worldez'
 # assert scramble(w1,w2) == False
