20a7caca8729529f863e6d14b52e856991b7dc20
cami <camilatienelabieberfever@gmail.com>
2022-04-19 22:14:02 -0300

    Adding string_n_iterations_string_00.py with 2 differents ways to think this problem but both ways aren't efficient and gives time out

---
+++ 5kyu/string_n_iterations_string/string_n_iterations_string_00.py
@@ -0,0 +1,32 @@
+# this is a 'solution' but it's not because it passed the test cases 
+# but not the general attempt
+# this solution gives time out in the attempt
+
+def get_evens(s):
+    return ''.join(char for idx,char in enumerate(s) if idx%2 == 0)
+
+def get_odds(s):
+    return ''.join(char for idx,char in enumerate(s) if idx%2 != 0)
+
+def jumbled_string(s, n):
+    s = get_evens(s) + get_odds(s)    
+    if n == 1:
+        return s
+    for num in range(1,n):
+        n -= 1
+        s = get_evens(s) + get_odds(s)
+    return s
+
+# this 'solution' doesn't attempt too, but also passed the test cases
+# its a recursive solution.
+# i think both solutions work but the 'n' parameter is too big for iterations
+# or maybe, the string its too large too.
+# I tried to expand the python recursion limit but it gives me time out too
+
+def jumbled_string(s, n):
+    even = ''.join(char for idx,char in enumerate(s) if idx%2 == 0)
+    odd = ''.join(char for idx,char in enumerate(s) if idx%2 != 0)
+    s = even + odd
+    if n == 1:
+        return s and -1
+    return jumbled_string(s,n-1)
