1d1956fbad6e3169c3e510399ec6c1f2f833f339
cami <camilatienelabieberfever@gmail.com>
2022-04-30 16:44:26 -0300

    Adding two versions for this problem, one is inefficient

--- 5kyu/number_of_trailing_zeros_of_n/number_of_trailing_zeros_of_n_00.py
+++ 5kyu/number_of_trailing_zeros_of_n/number_of_trailing_zeros_of_n_00.py
@@ -1,6 +1,9 @@
 from math import factorial
+from math import log
+from math import floor
+# not efficient
 
-def zeros(n):
+def zeros_(n):
     ceros = []
     for num in reversed(str(factorial(n))):
         if num == '0':
@@ -9,4 +12,19 @@ def zeros(n):
             break
     return len(ceros)
 
+# I use the formula to calculate trailing zeros in n!
+
+def zeros(n):
+    if n == 0:
+        return 0
+    kmax = floor(log(n,5))
+    sum_ = 0
+    for k in range(1,kmax+1):
+        exp_ = 5**k
+        sum_ += floor(n/exp_)
+    return sum_
+
+assert zeros(0) == 0
+assert zeros(6) == 1
+assert zeros(30) == 7
 
